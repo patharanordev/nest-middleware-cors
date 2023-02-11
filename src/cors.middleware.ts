2cefdef97c01788531fa85f4902c2e8a1ebed581
Pathara Norasethasopon <pathara.no@bdms.co.th>
2023-02-11 09:34:43 +0700

    Added debugging

--- src/cors.middleware.ts
+++ src/cors.middleware.ts
@@ -10,11 +10,19 @@ import { allowedOrigins } from './constants/allowed-origin';
 @Injectable()
 export class CorsMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: () => void) {
-    const host = req.headers.host;
-    console.log('requesgt from host:', host);
-    if (allowedOrigins.indexOf(host) > -1) {
-      console.log('allowed cors for:', host);
-      res.setHeader('Access-Control-Allow-Origin', host);
+    const origin = req.headers.origin;
+    // console.log('request from origin:', origin);
+    console.log('req.headers:', req.headers);
+    console.log('req.header:', req.header);
+    console.log('req.originalUrl:', req.originalUrl);
+    console.log('req.url:', req.url);
+    console.log('req.host:', req.host);
+    console.log('req.hostname:', req.hostname);
+    console.log('req.ips:', req.ips);
+    console.log('req.get(\'origin\'):', req.get('origin'));
+    if (allowedOrigins.indexOf(origin) > -1) {
+      console.log('allowed cors for:', origin);
+      res.setHeader('Access-Control-Allow-Origin', origin);
       res.setHeader('Access-Control-Allow-Methods', 'POST');
       res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Accept');
       next();
