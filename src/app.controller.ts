9a1ce8fab039f4c856d29f1018d2ef9a43d7341e
Pathara Norasethasopon <pathara.no@bdms.co.th>
2023-02-11 16:01:53 +0700

    Update app.controller.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,12 +2,13 @@ import { Controller, HttpStatus, Post, Res } from '@nestjs/common';
 import { AppService } from './app.service';
 import { allowedOrigins } from './constants/allowed-origin';
 
-@Controller({ host:allowedOrigins })
+// @Controller({ host:allowedOrigins })
+@Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Post()
-  getHello(@Res() res) {
+  async getHello(@Res() res) {
     return res.send({
       statusCode: HttpStatus.OK,
       message: this.appService.getHello()
