179ff973c5f4d47520e9d42713be4b7010bcbbea
Pathara Norasethasopon <pathara.no@bdms.co.th>
2023-02-11 11:29:15 +0700

    Try on middleware again

--- src/main.ts
+++ src/main.ts
@@ -7,38 +7,37 @@ import {
 import { AppModule } from './app.module';
 import { allowedOrigins } from './constants/allowed-origin'
 import { HttpException, HttpStatus } from '@nestjs/common';
-import cors from '@fastify/cors'
 
-const CORS_OPTIONS = {
-  origin: (origin, callback) => {
-    console.log('origin:', origin)
-    if (allowedOrigins.indexOf(origin) !== -1) {
-      console.log("allowed cors for:", origin)
-      callback(null, true)
-    } else {
-      console.log("blocked cors for:", origin)
-      callback(new HttpException('Host not allowed', HttpStatus.BAD_REQUEST))
-    }
-  },
-  // origin: allowedOrigins,
-  allowedHeaders: [
-    'Access-Control-Allow-Origin',
-    'Origin',
-    'X-Requested-With',
-    'Accept',
-    'Content-Type',
-    'Authorization',
-  ],
-  methods: "POST,OPTIONS",
-  credentials: true,
-}
+// const CORS_OPTIONS = {
+//   origin: (origin, callback) => {
+//     console.log('origin:', origin)
+//     if (allowedOrigins.indexOf(origin) !== -1) {
+//       console.log("allowed cors for:", origin)
+//       callback(null, true)
+//     } else {
+//       console.log("blocked cors for:", origin)
+//       callback(new HttpException('Host not allowed', HttpStatus.BAD_REQUEST))
+//     }
+//   },
+//   // origin: allowedOrigins,
+//   allowedHeaders: [
+//     'Access-Control-Allow-Origin',
+//     'Origin',
+//     'X-Requested-With',
+//     'Accept',
+//     'Content-Type',
+//     'Authorization',
+//   ],
+//   methods: "POST,OPTIONS",
+//   credentials: true,
+// }
 
 async function bootstrap() {
-  const adapter = new FastifyAdapter();
-  adapter.enableCors(CORS_OPTIONS)
-  const app = await NestFactory.create<NestFastifyApplication>(AppModule, adapter);
+  // const adapter = new FastifyAdapter();
+  // adapter.enableCors(CORS_OPTIONS)
+  // const app = await NestFactory.create<NestFastifyApplication>(AppModule, adapter);
 
-  // const app = await NestFactory.create<NestFastifyApplication>(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule);
 
   // app.enableCors(CORS_OPTIONS)
   await app.listen(3000, '0.0.0.0');
